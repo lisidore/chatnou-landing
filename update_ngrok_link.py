94785522a209e49744d5ee5040e2ba66f8fa9b7a
Lenn <lennisidore@gmail.com>
2025-07-02 20:49:38 -0400

    fix: point launch button to chatnou.com/select_user.html

--- update_ngrok_link.py
+++ update_ngrok_link.py
@@ -10,22 +10,6 @@ print(f"🌐 Current ngrok URL: {public_url}")
 # use the public_url as-is
 multiuser_url = public_url
 
-# update index.html
-with open("index.html", "r") as f:
-    index_html = f.read()
-
-pattern_index = r'href="http[s]?://[^/]+(/select_user\.html)"'
-new_index_html = re.sub(
-    pattern_index,
-    f'href="{multiuser_url}\\1"',
-    index_html
-)
-
-with open("index.html", "w") as f:
-    f.write(new_index_html)
-
-print("✅ index.html updated with new ngrok URL (keeping /select_user.html)")
-
 # update select_user.html
 with open("select_user.html", "r") as f:
     select_html = f.read()
@@ -40,4 +24,4 @@ new_select_html = re.sub(
 with open("select_user.html", "w") as f:
     f.write(new_select_html)
 
-print("✅ select_user.html updated with new ngrok URL (keeping /upload_xxx)")
+print("✅ select_user.html updated with new ngrok URL (keeping /upload_xxx')")
